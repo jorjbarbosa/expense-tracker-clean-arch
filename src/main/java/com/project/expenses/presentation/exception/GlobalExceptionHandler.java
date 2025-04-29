4ccc2350c1fa8b1e7875c3ca7b9e6b899fac826d
Jorge Barbosa <jorgebarbosa.dev@gmail.com>
2025-04-29 16:40:10 -0400

    Added GlobalExceptionHandler

---
+++ src/main/java/com/project/expenses/presentation/exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.project.expenses.presentation.exception;
+
+import com.project.expenses.application.exception.BussinessException;
+import com.project.expenses.presentation.dto.response.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BussinessException.class)
+    public ResponseEntity<ErrorDTO> handleBusinessException(BussinessException ex) {
+        ErrorDTO errorResponse = new ErrorDTO();
+        errorResponse.setMessage(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleGlobalException(Exception ex) {
+        ErrorDTO errorResponse = new ErrorDTO();
+        errorResponse.setMessage("Internal Error");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
